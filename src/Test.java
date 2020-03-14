ec602f7622f891e492aaaf7f49936ca3d5b14966
sunkangchao <928893296@qq.com>
2020-03-15 02:43:09 +0800

    Merge branch 'test'
    # Conflicts:
    #	.idea/vcs.xml
    #	src/Test3.java

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello git!");
-        System.out.println("我爱你java!");
+        System.out.println("验证是否能够正常拉取最新版本。");
     }
 
 }
